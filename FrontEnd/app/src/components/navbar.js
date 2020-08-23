64f836706e6c23d8ee30e2e19df663a331818c46
Kevin Tena <kevten09@gmail.com>
2020-08-22 23:54:06 -0500

    Added placeholder text for pnavbar and set up tailwindcss files for styling

--- FrontEnd/app/src/components/navbar.js
+++ FrontEnd/app/src/components/navbar.js
@@ -4,9 +4,14 @@ const Navbar = () => {
     return (
         <div>
             <div>
-            <h2>Curatem</h2>Curatem
-            </div>
-            </div>
+                 <h2>Curatem</h2>
+                 <div> Search Bar PH</div>
+                 <div> Categories </div>
+                 <div> My Paths </div>
+                 <button>Log In</button>
+                 <button>Sign Up</button>
+             </div>
+         </div>
         )
 }
 

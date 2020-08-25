5242b91ad9e9dc03e15267a1cb46e3affd4c9f2a
Kevin Tena <kevten09@gmail.com>
2020-08-24 23:59:10 -0500

    Used flexbox to space divs

--- FrontEnd/app/src/components/navbar.js
+++ FrontEnd/app/src/components/navbar.js
@@ -3,13 +3,15 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            <div>
+            <div class="flex justify-evenly">
                  <h2>Curatem</h2>
                  <div> Search Bar PH</div>
                  <div> Categories </div>
                  <div> My Paths </div>
-                 <button>Log In</button>
-                 <button>Sign Up</button>
+                 <div class="flex justify-evenly w-1/12">
+                    <button>Log In</button>
+                    <button>Sign Up</button>
+                 </div>
              </div>
          </div>
         )

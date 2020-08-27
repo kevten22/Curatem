92f47709dc47af46cf19d5219ccbe20d89866398
Kevin Tena <kevten09@gmail.com>
2020-08-26 23:53:30 -0500

    Organized nav into seperate divs for better spacing

--- FrontEnd/app/src/components/navbar.js
+++ FrontEnd/app/src/components/navbar.js
@@ -6,13 +6,16 @@ const Navbar = () => {
         <div>
             <div class="flex justify-between mx-10">
                  <img class='-space-x-20' src={Logo} alt='Curatem Logo'></img>
-                 <div> Categories </div>
-                 <input type="text" name="name" placeholder="Search" /> 
-                 <div> My Paths </div>
-                 <div class="flex justify-between w-1/12">
+                 <div class='flex justify-between w-1/4'> 
+                     <h3 class="self-center"> Categories
+                      </h3>
+                      <input type="text" name="name" placeholder="Search" /> 
+                    </div>
+                 <div class="flex justify-between w-2/12 h-10 self-center">
+                    <h4 class="self-center"> My Paths </h4>
                     <button>Log In</button>
                     <button>Sign Up</button>
-                 </div>
+                  </div>
              </div>
          </div>
         )

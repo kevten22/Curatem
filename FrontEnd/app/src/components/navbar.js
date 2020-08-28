9bf3cf10fe2cfba31eeaa82b992360ba1005b984
Kevin Tena <kevten09@gmail.com>
2020-08-27 23:15:57 -0500

    Added basic styling to the search bar and login/signup buttons

--- FrontEnd/app/src/components/navbar.js
+++ FrontEnd/app/src/components/navbar.js
@@ -3,18 +3,18 @@ import Logo from './Curatem Logo.jpg';
 
 const Navbar = () => {
     return (
-        <div>
-            <div class="flex justify-between mx-10">
+        <div class='border-b'>
+            <div class='flex justify-between mx-10 pb-2'>
                  <img class='-space-x-20' src={Logo} alt='Curatem Logo'></img>
                  <div class='flex justify-between w-1/4'> 
-                     <h3 class="self-center"> Categories
+                     <h3 class='self-center'> Categories
                       </h3>
-                      <input type="text" name="name" placeholder="Search" /> 
+                      <input class='self-center h-10 bg-gray-300' type='text' name='name' placeholder='Search' /> 
                     </div>
-                 <div class="flex justify-between w-2/12 h-10 self-center">
-                    <h4 class="self-center"> My Paths </h4>
-                    <button>Log In</button>
-                    <button>Sign Up</button>
+                 <div class='flex justify-between w-2/12 h-8 self-center'>
+                    <h4 class='self-center'> My Paths </h4>
+                    <button class='p-1 border border-gray-500 hover:bg-blue-900 hover:text-gray-100'>Log In</button>
+                    <button class='p-1 bg-gray-500 text-gray-100 hover:bg-blue-900'>Sign Up</button>
                   </div>
              </div>
          </div>

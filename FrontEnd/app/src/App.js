3ee720f3a9e329893a3aea013170d9d0162a1a11
Kevin Tena <kevten09@gmail.com>
2020-08-29 18:40:29 -0500

    Create files and boilerplate for Categories and MyPaths pages. Add react router links in navbar component for Home, Categories, and My Paths pages

--- FrontEnd/app/src/App.js
+++ FrontEnd/app/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import NavBar from './components/navbar';
 import Home from './pages/Home';
+import Categories from './pages/Categories';
+import MyPaths from './pages/MyPaths'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Route exact path="/" component={Home} />
+      <Route path="/categories" component={Categories} />
+      <Route path="/mypaths" component={MyPaths} />
     </div>
   );
 }
--- FrontEnd/app/src/components/navbar.js
+++ FrontEnd/app/src/components/navbar.js
@@ -1,18 +1,28 @@
 import React from "react";
 import Logo from './Curatem Logo.jpg';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div class='border-b'>
             <div class='flex justify-between mx-10 pb-2'>
-                 <img class='-space-x-20' src={Logo} alt='Curatem Logo'></img>
-                 <div class='flex justify-between w-1/4'> 
-                     <h3 class='self-center'> Categories
+                <Link to="/">
+                    <img class='-space-x-20' src={Logo} alt='Curatem Logo'></img>
+                 </Link>
+                 <div class='flex justify-between w-1/4'>
+                     <h3 class='self-center'> 
+                        <Link to="/categories">
+                            Categories
+                         </Link>
                       </h3>
                       <input class='self-center h-10 bg-gray-300' type='text' name='name' placeholder='Search' /> 
                     </div>
                  <div class='flex justify-between w-2/12 h-8 self-center'>
-                    <h4 class='self-center'> My Paths </h4>
+                    <h4 class='self-center'>
+                        <Link to="/mypaths"> 
+                        My Paths 
+                        </Link>
+                     </h4>
                     <button class='p-1 border border-gray-500 hover:bg-blue-900 hover:text-gray-100'>Log In</button>
                     <button class='p-1 bg-gray-500 text-gray-100 hover:bg-blue-900'>Sign Up</button>
                   </div>

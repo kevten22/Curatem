2796b855c6b2373934d5bfd3893b6ba3969c1a80
Kevin Tena <kevten09@gmail.com>
2020-09-06 23:10:24 -0500

    Added register route for username

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import bcrypt
 import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/curatem"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres123@localhost:5433/Curatem"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class UserModel(db.Model):
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -20,14 +21,27 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
+    def __repr__(self):
+        return f"<User {self.name}>"
+
+@app.route('/register', methods=['POST'])
+def register():
+    try:
+        username = request.json.get('username', None)
+        password = request.json.get('password', None)
+
+        user = User(username=username,password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return'success', 200
+
+    except AttributeError:
+        return 'Provide a Username and Password in JSON format in the request body', 400
+
 @app.route('/')
 def hello():
     return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':

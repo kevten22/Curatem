5e1c4dff5ac0e032c3e7e0b87e00f6843ccbac05
Kevin Tena <kevten09@gmail.com>
2020-09-23 23:57:43 -0500

    Add dotenv code to config and curatem.py for app factory usage

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -5,15 +5,14 @@ from app.routes import bp
 @bp.route('/register', methods=['POST'])
 def register():
     try:
-        username = request.json.get('userna
-        me', None)
+        username = request.json.get('username', None)
         password = request.json.get('password', None)
 
         user = User(username=username,password=password)
         db.session.add(user)
         db.session.commit()
 
-        return'success', 200
+        return 'success', 200
 
     except AttributeError:
         return 'Provide a Username and Password in JSON format in the request body', 400

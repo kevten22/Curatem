127a28ffdc97aab2310a80ac46ac0f21b8cd333e
Kevin Tena <kevten09@gmail.com>
2020-11-09 23:55:54 -0600

    Implement Levels model and schema

--- backend/app/models/path_models.py
+++ backend/app/models/path_models.py
@@ -11,4 +11,12 @@ class Path(db.Model):
     dislikes = db.Column(db.Integer)
     followers = db.relationship('User', secondary="followers")
     users_completed = db.relationship('User', secondary="paths_completed")
+    levels = db.relationship('Level')
     
+class Level(db.Model):
+    __tablename__ = 'levels'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    difficulty = db.Column(db.String())
+    path = db.Column(db.Integer, db.ForeignKey('paths.id'))
+    following_level = db.Column(db.Integer, db.ForeignKey('levels.id'))
